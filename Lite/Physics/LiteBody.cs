b997d125df7fb8cb9ca347a1dfbd6b01cc1ea023
DanielGH3 <danielnazuni@gmail.com>
2023-08-30 12:33:35 +0300

    Added support for transforming boxes (rotation, position)

--- Lite/Physics/LiteBody.cs
+++ Lite/Physics/LiteBody.cs
@@ -31,6 +31,11 @@ namespace Lite.Physics
         public readonly float Width;
         public readonly float Height;
 
+        private readonly LiteVector[] vertices;
+        public int[] Triangles;
+        private LiteVector[] transformedVertices;
+        private bool transformUpdateRequired;
+
         public readonly ShapeType ShapeType;
 
         public LiteVector Position
@@ -58,6 +63,79 @@ namespace Lite.Physics
             this.Width = width;
             this.Height = height;
             this.ShapeType = shapeType;
+
+            if (this.ShapeType == ShapeType.Box)
+            {
+                this.vertices = LiteBody.CreateBoxVertices(this.Width, this.Height);
+                this.Triangles = CreateBoxTriangles();
+                this.transformedVertices = new LiteVector[this.vertices.Length];
+            }
+            else
+            {
+                this.vertices = null;
+                this.Triangles = null;
+                this.transformedVertices = null;
+            }
+
+            this.transformUpdateRequired = true;
+        }
+
+        public LiteVector[] GetTransformedVertices()
+        {
+            if (this.transformUpdateRequired)
+            {
+                LiteTransform transfrom = new LiteTransform(this.position, this.rotation);
+
+                for(int i=  0; i<this.vertices.Length; i++)
+                {
+                    LiteVector v = this.vertices[i];
+                    this.transformedVertices[i] = LiteVector.Transform(v, transfrom);
+                }
+
+                this.transformUpdateRequired = false;
+            }
+
+            return this.transformedVertices;
+        }
+
+        public void Move(LiteVector amount)
+        {
+            this.position += amount;
+            this.transformUpdateRequired = true;
+        }
+
+        public void Rotate(float amount)
+        {
+            this.rotation += amount;
+            this.transformUpdateRequired = true;
+        }
+
+        private static LiteVector[] CreateBoxVertices(float width, float height)
+        {
+            float left = -width / 2;
+            float right = left + width;
+            float top = -height / 2;
+            float bottom = top + height;
+
+            LiteVector[] vertices = new LiteVector[4];
+            vertices[0] = new LiteVector(left, top);
+            vertices[1] = new LiteVector(right, top);
+            vertices[2] = new LiteVector(right, bottom);
+            vertices[3] = new LiteVector(left, bottom);
+
+            return vertices;
+        }
+
+        private static int[] CreateBoxTriangles()
+        {
+            int[] triangles = new int[6];
+            triangles[0] = 0;
+            triangles[1] = 1;
+            triangles[2] = 2;
+            triangles[3] = 0;
+            triangles[4] = 2;
+            triangles[5] = 3;
+            return triangles;
         }
 
         public static bool CreateCircleBody(float radius, LiteVector position, float density, float restitution, bool isStatic, out LiteBody body, out string error)
