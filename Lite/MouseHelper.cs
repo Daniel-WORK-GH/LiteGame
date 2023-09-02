143b1ac6316684571e3e33f12adde29156da3fdd
Danielnaz <danielnaz69@gmail.com>
2023-09-02 22:46:46 +0300

    Added functionality to get mouse actions quickly

---
+++ Lite/MouseHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lite
+{
+    public static class MouseHelper
+    {
+        private static MouseState currentMouseState;
+        private static MouseState previousMouseState;
+
+        public static void Update()
+        {
+            previousMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+        }
+
+        public static bool IsLeftClick()
+        {
+            return currentMouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released;
+        }
+
+        public static bool IsRightClick()
+        {
+            return currentMouseState.RightButton == ButtonState.Pressed && previousMouseState.RightButton == ButtonState.Released;
+        }
+    }
+
+}
