625ec39b44d9f76986a7d13b3418d9a9c8db3d38
Danielnaz <danielnaz69@gmail.com>
2023-09-02 22:46:22 +0300

    Added fucntions to sort all physics bodies into one class that handles them

--- Lite/Physics/LiteWorld.cs
+++ Lite/Physics/LiteWorld.cs
@@ -6,12 +6,115 @@ using System.Threading.Tasks;
 
 namespace Lite.Physics
 {
-    internal class LiteWorld
+    public class LiteWorld
     {
         public const float MinBodySize = 0.01f * 0.1f;
         public const float MaxBodySize = 64f * 64f;
 
         public const float MinDensity = 0.5f; // g/cm^3
         public const float MaxDensity = 22f;
+
+        private List<LiteBody> bodylist;
+        private LiteVector gravity;
+
+        public int BodyCount
+        {
+            get { return this.bodylist.Count; }
+        }
+
+        public LiteWorld()
+        {
+            this.gravity = new LiteVector(0f, 9.81f);
+            this.bodylist = new List<LiteBody>();
+        }
+
+        public void AddBody(LiteBody body)
+        {
+            this.bodylist.Add(body);
+        }
+
+        public bool RemoveBody(LiteBody body)
+        {
+            return this.bodylist.Remove(body);
+        }
+
+        public bool GetBody(int index, out LiteBody body)
+        {
+            if(index < 0 || index >= this.bodylist.Count)
+            {
+                body = null;
+                return false;
+            }
+
+            body = this.bodylist[index];
+            return true;
+        }
+
+        public void Step(float time)
+        {
+            // Movement step
+            for (int i = 0; i < this.bodylist.Count; i++)
+            {
+                this.bodylist[i].Step(time);
+            }
+
+            // Collision step
+            for (int i = 0; i < this.bodylist.Count; i++)
+            {
+                LiteBody bodyA = this.bodylist[i];
+
+                for (int j = i + 1; j < this.bodylist.Count; j++)
+                {
+                    LiteBody bodyB = this.bodylist[j];
+
+                    if(Collide(bodyA, bodyB, out LiteVector normal, out float depth))
+                    {
+                        bodyA.Move(-normal * depth / 2);   
+                        bodyB.Move(normal * depth / 2);
+                    }
+                }
+            }
+        }
+
+        private bool Collide(LiteBody bodyA, LiteBody bodyB, out LiteVector normal, out float depth)
+        {
+            normal = LiteVector.Zero;
+            depth = 0;
+
+            ShapeType shapeTypeA = bodyA.ShapeType;
+            ShapeType shapeTypeB = bodyB.ShapeType;
+
+            if(shapeTypeA is ShapeType.Box)
+            {
+                if(shapeTypeB is ShapeType.Box)
+                {
+                    return Collisions.IntersectPolygons(bodyA.GetTransformedVertices(), bodyB.GetTransformedVertices(), 
+                        out normal, out depth);
+                }
+                else if(shapeTypeB == ShapeType.Circle)
+                {
+                    bool result = Collisions.IntersectCirclePolygon(bodyB.Position, bodyB.Raidus, bodyA.GetTransformedVertices(),
+                        out normal, out depth);
+
+                    normal = -normal;
+                    return result;
+                }
+            }
+            if (shapeTypeA is ShapeType.Circle)
+            {
+                if (shapeTypeB is ShapeType.Box)
+                {
+                    return Collisions.IntersectCirclePolygon(bodyA.Position, bodyA.Raidus, bodyB.GetTransformedVertices(),
+                        out normal, out depth);
+                }
+                else if (shapeTypeB == ShapeType.Circle)
+                {
+                    return Collisions.IntersectCircles(bodyA.Position, bodyA.Raidus, bodyB.Position, bodyB.Raidus,
+                        out normal, out depth);
+                }
+            }
+
+            return false;
+        }
     }
 }
