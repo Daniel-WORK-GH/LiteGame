f789525029dca52b5cbcf5c7dc3f00b95ac0d458
DanielGH3 <danielnazuni@gmail.com>
2023-09-05 00:13:16 +0300

    Get screen rectangle bounds

--- Lite/Graphics/Screen.cs
+++ Lite/Graphics/Screen.cs
@@ -21,5 +21,31 @@ namespace Lite.Graphics
             this.Width = width;
             this.Height = height;
         }
+
+        public Rectangle CalculateDestinationRectangle()
+        {
+            Rectangle backbufferRectangle = this.game.GraphicsDevice.PresentationParameters.Bounds;
+            float backbuffer_aspect = (float)backbufferRectangle.Width / (float)backbufferRectangle.Height;
+            float screen_aspect = (float)this.Width / (float)this.Height;
+
+            float rx = 0;
+            float ry = 0;
+            float rw = backbufferRectangle.Width;
+            float rh = backbufferRectangle.Height;
+
+            if (screen_aspect > backbuffer_aspect)
+            {
+                rh = rw / screen_aspect;
+                ry = (backbufferRectangle.Height - rh) / 2f;
+            }
+            else if (screen_aspect < backbuffer_aspect)
+            {
+                rw = rh * screen_aspect;
+                rx = (backbufferRectangle.Width - rw) / 2f;
+            }
+
+            Rectangle result = new Rectangle((int)rx, (int)ry, (int)rw, (int)rh);
+            return result;
+        }
     }
 }
